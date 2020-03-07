3ab0ed9a87f9a82aa8914060bb8c29c836ba384e
jscodebit <soujanya.janapatla@gmail.com>
2020-03-06 22:01:33 -0600

    Modified header and script in index and application files

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,26 @@ function sphereSurfaceArea(radius){
     let volumeElement = document.getElementById("volumeResult");
     volumeElement.value = volumeResult;
   }
+
+  var output = document.getElementById('output');
+        var buttonClick = document.getElementById('loadNew')
+        buttonClick.addEventListener('click',function(){
+            loadAjax();
+        })
+        function loadAjax(){
+        var ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function(){
+            //console.log('state changed',ajax.readyState,ajax.status)
+            if(ajax.readyState == 4 && ajax.status==200){
+                   // console.log(ajax.responseText)
+                var json = JSON.parse(ajax.responseText);
+                 var data = json.results[0];
+         
+                var message = '<h2>'+data.name.first + '  ' + data.name.last + '</h2><img src="'+data.picture.large +'">'
+               
+                 output.innerHTML = message;
+            }
+        }
+        ajax.open('GET','https://randomuser.me/api/',true)
+        ajax.send();
+        }
